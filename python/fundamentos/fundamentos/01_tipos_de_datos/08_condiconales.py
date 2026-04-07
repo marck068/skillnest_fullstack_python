3fcbc2dcf26b35d55a317b352f6f32a65abbacc7
sebast1an-bust4mante <marcelorios@liceovvh.cl>
2026-04-07 11:46:49 -0400

    asd

---
+++ python/fundamentos/fundamentos/01_tipos_de_datos/08_condiconales.py
@@ -0,0 +1,33 @@
+#Condicionales
+#Estructura else - if
+num = 15
+if num > 20:
+   print("Número mayor a 20")
+else:
+   print("Número menor a 20")
+'''
+La variable num es menor a 20, por lo que el bloque de código de else será ejecutado.
+Es decir que vamos a imprimir "Número menor a 20"
+'''
+#Estrucutra if - elif - else
+num = 101
+if num > 50:
+   print("Número mayor a 50")
+elif num > 100:
+   print("Número mayor a 100")
+else:
+   print("Número menor a 10")
+'''
+A pesar de que num es mayor que 50 y 100, la primera condicional
+que se cumpla es la única que se ejecutará.
+Es por eso que solo imprimirá: "Número mayor a 50"
+'''
+if num < 60:
+   print("Número menor a 50") #Nunca entramos a esta línea de código
+
+#No se cumple con la condicional, por lo que no se ejecuta el bloque de código
+
+#Tarea desafío
+#Ingresar 3 números por teclado e identificar cual es el mayor y cual 
+# es el menor. Mostrar ambos.
+

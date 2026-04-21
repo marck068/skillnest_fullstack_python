086d17376d4e892674b312bbb8e639df3f8b3bc9
sebast1an-bust4mante <marcelorios@liceovvh.cl>
2026-04-21 12:58:25 -0400

    asd

--- python/fundamentos/fundamentos/02_estructuras_control/14_funciones_basicas_practica_2.py
+++ python/fundamentos/fundamentos/02_estructuras_control/14_funciones_basicas_practica_2.py
@@ -2,20 +2,18 @@
 
 #Ejercicio 1
 # Calcula experiencia
-def  multiplica_por_2():
-    return [0, 2, 4, 6, 8, 10]
+def multiplica_por_2(n):
+    return [i * 2 for i in range(n + 1)]
 
-multiplica_por_2(5)
+print(multiplica_por_2(5))
 # Debe retornar: [0, 2, 4, 6, 8, 10]
 
 
 #Ejercicio 2
 # Analiza publicaciones
 def suma_y_resta():
-    print(f"{suma_y_resta[0]}")
-    return suma_y_resta[1]
+    suma_y_resta([120, 115])
 
-suma_y_resta([120, 115])
 # Imprime: 235 y retorna: 5
 
 

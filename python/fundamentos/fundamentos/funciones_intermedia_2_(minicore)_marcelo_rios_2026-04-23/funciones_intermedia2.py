46d238cbb5b9f40d485af76e16fc9b41ef0784c9
sebast1an-bust4mante <marcelorios@liceovvh.cl>
2026-04-23 14:07:51 -0400

    asdasd

--- python/fundamentos/fundamentos/funciones_intermedia_2_(minicore)_marcelo_rios_2026-04-23/funciones_intermedia2.py
+++ python/fundamentos/fundamentos/funciones_intermedia_2_(minicore)_marcelo_rios_2026-04-23/funciones_intermedia2.py
@@ -7,10 +7,13 @@ datos = [
 # 1. Cambiar el puntaje de Pedro a 75
 datos[2]["puntaje"] = 75
 print(datos)
+
 # 2. Crear función que imprima:
 #    "Carlos obtuvo 80 puntos"
+print(f"{datos[0]['nombre']} obtuvo {datos[0]['puntaje']} puntos")
+
+# 3. Crear función que reciba "nombre" o "puntaje" e imprima solo esos valores
 def carlo(lista):
     for i in lista:
         print(f"{i['nombre']} obtuvo {i['puntaje']}")
-# 3. Crear función que reciba "nombre" o "puntaje" e imprima solo esos valores
-
+carlo(datos)

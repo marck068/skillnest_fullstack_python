7de8ed4e2adbe94012cc8b739e8fbc8ef087dac5
sebast1an-bust4mante <marcelorios@liceovvh.cl>
2026-05-11 12:04:20 -0400

    asdasd

--- python/core/Suscripción_plataforma_streaming_(Core)_marcelo_rios_2026-05-07/suscripcion_streaming.py
+++ python/core/Suscripción_plataforma_streaming_(Core)_marcelo_rios_2026-05-07/suscripcion_streaming.py
@@ -1,29 +1,18 @@
 #Atributos, métodos de clase, métodos estáticos
 
-#DEFINICION DE LA CLASE
 class SuscripcionStreaming:
-    #Atributo de clase
-    costos_suscripcion = {
-        "Gratis": 0,
-        "Estándar": 5.99,
-        "Premium": 10.99
-    }
+    costos_suscripcion = {"Gratis": 0, "Estándar": 5.99, "Premium": 10.99}
 
-    #Lista en donde esten todas las suscripciones
     suscripciones = []
 
-    #Método CONSTRUCTOR
     def __init__(self, usuario, tipo_suscripcion="Gratis"):
-        #Atributos de instancia
         self.usuario = usuario
         self.tipo_suscripcion = tipo_suscripcion
         self.costo_mensual = SuscripcionStreaming.costos_suscripcion[tipo_suscripcion]
         self.saldo_pendiente = self.costo_mensual
 
-        #Agregar elementos a la lista SuscripcionStreaming (objeto)
         SuscripcionStreaming.suscripciones.append(self)
 
-    #Método de instancia
     def realizar_pago(self, monto):
         """Reduce el saldo pendiente según el monto pagado."""
         self.saldo_pendiente = self.saldo_pendiente - monto
@@ -31,7 +20,6 @@ class SuscripcionStreaming:
         print(f"Pago realizado por: {self.usuario}")
         print(f"Saldo pendiente: {self.saldo_pendiente}")
 
-    #Método de instancia
     def cambiar_suscripcion(self, nuevo_tipo):
         """Cambia el tipo de suscripción y actualiza el costo mensual."""
         self.tipo_suscripcion = nuevo_tipo
@@ -40,8 +28,6 @@ class SuscripcionStreaming:
 
         print(f"Nueva suscripción: {self.tipo_suscripcion}")
 
-    #Método estático
-    #Este no usa CLS ni SELF, solo parámetros.
     @staticmethod
     def ver_contenido_exclusivo(tipo_suscripcion):
         """Permite ver contenido exclusivo según el tipo de suscripción."""
@@ -52,7 +38,6 @@ class SuscripcionStreaming:
         else:
             print("No tiene acceso a contenido exclusivo")
 
-    #Método de instancia
     def mostrar_info_suscripcion(self):
         """Muestra la información de la suscripción del usuario."""
         print(f"Usuario: {self.usuario}")
@@ -60,21 +45,16 @@ class SuscripcionStreaming:
         print(f"Costo mensual: {self.costo_mensual}")
         print(f"Saldo pendiente: {self.saldo_pendiente}")
 
-    #Método de CLASE
-    # Usa "CLS" porque trabaja con la información de la clase
     @classmethod
     def cantidad_suscripciones(cls):
         return len(cls.suscripciones)
 
-#Creación de objetos (Instancias)
 s1 = SuscripcionStreaming("Daniel")
 s2 = SuscripcionStreaming("Randy", "Estándar")
 s3 = SuscripcionStreaming("Yoycer", "Premium")
 
-#Uso de métodos de instancia
 print("== MÉTODO DE INSTANCIA==")
 
-#Mostrar datos de suscripciones
 s1.mostrar_info_suscripcion()
 print()
 
@@ -84,7 +64,6 @@ print()
 s3.mostrar_info_suscripcion()
 print()
 
-#Uso de método de instancia
 print("== REALIZAR PAGOS ==")
 
 s2.realizar_pago(3)
@@ -93,13 +72,11 @@ print()
 s3.realizar_pago(5)
 print()
 
-#Uso de método de instancia
 print("== CAMBIAR SUSCRIPCIÓN ==")
 
 s1.cambiar_suscripcion("Premium")
 print()
 
-#Método estático
 print("=== MÉTODO ESTÁTICO ===")
 
 SuscripcionStreaming.ver_contenido_exclusivo(s1.tipo_suscripcion)
@@ -111,7 +88,6 @@ print()
 SuscripcionStreaming.ver_contenido_exclusivo(s3.tipo_suscripcion)
 print()
 
-#Contar suscripciones
 print("== CONTAR SUSCRIPCIONES ==")
 
 print(f"Total suscripciones: {SuscripcionStreaming.cantidad_suscripciones()}")

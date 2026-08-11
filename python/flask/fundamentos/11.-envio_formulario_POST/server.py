16069a834fe61a6dd0ba37b5e8dfb68253aef83c
sebast1an-bust4mante <marcelorios@liceovvh.cl>
2026-08-11 11:47:45 -0400

    asdasd

--- python/flask/fundamentos/11.-envio_formulario_POST/server.py
+++ python/flask/fundamentos/11.-envio_formulario_POST/server.py
@@ -46,17 +46,13 @@ def index():
 
 @app.route("/crear_usuario", methods=["POST"])
 def crear_usuario():
-
     print("========== NUEVO USUARIO ==========")
-
     print(request.form)
-
     print("-----------------------------------")
-
     print("Nombre:", request.form["nombre"])
-
     print("Correo:", request.form["email"])
-
+    print("Edad:", request.form["edad"])
+    print("Ciudad:", request.form["ciudad"])
     print("===================================")
 
     # Nunca renderizamos una plantilla
